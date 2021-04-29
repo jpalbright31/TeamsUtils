d4cab0a463e06580bbe280d6376f580927a30838
dikahdoff <75095169+dikahdoff@users.noreply.github.com>
2021-04-29 18:04:57 +0200

    Added automatic channel switcher
    and added description.

--- scripts/AutoJoinMeeting.js
+++ scripts/AutoJoinMeeting.js
@@ -4,7 +4,7 @@
 
 /*
 After you've joined a Meeting press Ctrl+Shift+I and paste the whole code (Ctrl+A) into the console section of the window and press enter.
-
+Automatically switches to the set channel and joins the meeting after the below set delay, if a meeting is to be started.
 
 If you want to manually stop the script, write 'cancelJoining()' then press enter in the console window.
 If you want to restart the script, write 'joiningAction()' then press enter in the console window.
@@ -20,6 +20,11 @@ var joinDelay = 1000;
 // Delay to wait before joining. (in milliseconds)
 var joinWait = 5000;
 
+// Switch to a team before searching.
+var switchChannel = false;
+var switchTo = "TeamName";
+var switchToChannel = "ChannelName";
+
 ///////////////////
 
 function sleep(ms) {
@@ -28,10 +33,49 @@ function sleep(ms) {
 
 // Loop
 var dojoining = true;
+var didswitch = false;
 async function joiningAction() {
     dojoining = true;
     console.clear()
     while(dojoining) {
+        if(switchChannel && !didswitch) {
+            console.warn("Switching to " + switchTo + " before fetching...\nDownload this script: https://github.com/dikahdoff/TeamsUtils");
+            var teams = document.getElementsByClassName("app-left-rail-width");
+            if(teams.length > 0) {
+                teams = teams[0].children[0].children[0].children[1].children;
+                if(teams.length > 0) {
+                    for(var i = 0; i < teams.length; i++) {
+                        if(teams[i].classList.contains("team")) {
+                            var cTeamName = teams[i].children[0].children[0].children[0].children[1].children[0].children[0].innerHTML;
+                            if(cTeamName.toLowerCase().contains(switchTo.toLowerCase())) {
+                                console.log("Found " + switchTo + " team, opening if necessary...\nDownload this script: https://github.com/dikahdoff/TeamsUtils");
+                                // Check if teams is collapsed, if it is, open it.
+                                if(teams[i].getAttribute("aria-expanded") == "false") {
+                                    console.log(teams[i]);
+                                    teams[i].children[0].children[0].click();
+                                }
+                                await sleep(50);
+                                // Click on the channel.
+                                var channels = teams[i].children[0].children[1].children[0].children[0].children;
+                                for(var j = 0; j < channels.length; j++) {
+                                    var channelName = channels[j].children[0].children[0].children[0].innerHTML;
+                                    if(channelName.toLowerCase().contains(switchToChannel.toLowerCase())) {
+                                        console.log("Found " + switchToChannel + " channel, opening if necessary...\nDownload this script: https://github.com/dikahdoff/TeamsUtils");
+                                        channels[j].children[0].click();
+                                        didswitch = true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    console.error("You don't have any teams to join to.\nDownload this script: https://github.com/dikahdoff/TeamsUtils");
+                }
+            } else {
+                console.error("Couldn't find teams list.\nDownload this script: https://github.com/dikahdoff/TeamsUtils");
+            }
+            await sleep(joinDelay);
+        }
         console.log("Fetching meetings...\nDownload this script: https://github.com/dikahdoff/TeamsUtils");
         // Get meeting button
         var element = document.getElementsByClassName("ts-sym ts-btn ts-btn-primary inset-border icons-call-jump-in ts-calling-join-button app-title-bar-button app-icons-fill-hover call-jump-in");
@@ -43,7 +87,7 @@ async function joiningAction() {
             await sleep(joinWait);
             console.log("Joining...\nDownload this script: https://github.com/dikahdoff/TeamsUtils");
             element.click();
-            await sleep(50);
+            await sleep(100);
             // Check if the continue without camera or audio dialog opens
             var withoutBtn = document.getElementsByClassName("ts-btn ts-btn-fluent ts-btn-fluent-secondary-alternate");
             if(withoutBtn.length > 0) {
@@ -51,7 +95,7 @@ async function joiningAction() {
                 withoutBtn[0].click();
                 console.warn("Closed warning box.\nDownload this script: https://github.com/dikahdoff/TeamsUtils");
             }
-            await sleep(50);
+            await sleep(100);
             var toggleMic = document.getElementById("preJoinAudioButton");
             // Check if toggle mic exists
             if(toggleMic != null) {
